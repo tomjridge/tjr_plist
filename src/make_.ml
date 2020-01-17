5be997f091da83a08f351f365a608b071aa10832
Tom Ridge <tom.j.ridge@googlemail.com>
2020-01-17 18:36:21 +0000

    +

--- src/make_.ml
+++ src/make_.ml
@@ -1,6 +1,6 @@
-(** We store the list of elts from offset 9 (say); the nxt pointer
-   takes up the first 9 bytes. Assumes max size of marshalled blk_ptr
-   is 9 bytes *)
+(** We store the list of elts from offset off0 (say); the nxt pointer
+   takes up the first off0 bytes. Assumes max size of marshalled blk_ptr
+   is off0 bytes *)
 open Plist_intf
 
 [@@@warning "-26"] (* FIXME *)
@@ -20,10 +20,15 @@ type ('a,'blk_id,'blk,'buf) marshal_info = {
   
 }
 
-(** Internal version *)
-module Internal = struct
-
-  let make (type buf blk_id blk t) = fun 
+module Make(S:sig 
+    type buf
+    type blk_id
+    type blk
+    type t
+  end)
+= struct
+  open S
+  let make = fun 
     ~monad_ops
     ~(buf_ops:buf buf_ops)
     ~(blk_ops:blk blk_ops)
@@ -38,20 +43,23 @@ module Internal = struct
       let { max_elt_sz; max_blk_id_sz; m_elt; u_elt; 
             m_blk_id; u_blk_id; blk_to_buf; buf_to_blk } = marshal_info in
       let buf_sz = Blk_sz.to_int blk_sz in
+      let buf_space ~off = buf_sz - off in
+      let can_fit ~off ~n = off+n<=buf_sz in
       
-      let elts_offset = max_blk_id_sz in
-      assert(elts_offset + max_elt_sz <= buf_sz);        
+      let elts_offset0 = max_blk_id_sz in
+      assert(can_fit ~off:elts_offset0 ~n:(2*max_elt_sz)); 
+      (* can fit 2 elts at least - including the None end of list marker *)
 
       let empty_blk () = String.make buf_sz chr0 |> of_string in
 
       (* NOTE returns the offset pointing to the None end-of-list marker *)
-      let x_to_buffer ~elts ~nxt : buf*int = 
+      let x_to_buf (elts,nxt) : buf*int = 
         (* write nxt *)
         create buf_sz |> fun buf ->
         (buf,0) |> m_blk_id nxt |> fun (buf,i) -> 
         assert(i<=max_blk_id_sz);
-        (buf,elts_offset,elts) |> iter_k (fun ~k (buf,off,elts) ->
-            assert(off+max_elt_sz < buf_sz);
+        (buf,elts_offset0,elts) |> iter_k (fun ~k (buf,off,elts) ->
+            assert(can_fit ~off ~n:max_elt_sz);
             match elts with 
             | [] -> 
               (* note the use of the offset just before the None *)
@@ -63,43 +71,111 @@ module Internal = struct
       
       let buf_to_x buf = 
         0 |> u_blk_id buf |> fun (nxt,_) -> 
-        (elts_offset,[]) |> iter_k (fun ~k (off,acc) -> 
+        (elts_offset0,[]) |> iter_k (fun ~k (off,acc) -> 
             u_elt buf off |> fun (e,off) -> 
             match e with 
             | None -> (List.rev acc,off)  (* note return pointer to None *)
             | Some e -> k (off,e::acc))
         |> fun (es,off) -> 
         es,nxt
-      in            
-        
-      let empty blk_id = 
-        let elts = [] in
-        let tl_nxt = None in
-        let tl_buffer = x_to_buffer ~elts ~nxt:tl_nxt in
+      in
+
+      let x_to_blk (elts,nxt) = 
+        x_to_buf (elts,nxt) |> fun (buf,_) -> buf_to_blk buf
+      in
+      let blk_to_x blk = blk_to_buf blk |> buf_to_x in      
+
+      let ops1 = { unmarshal=blk_to_x; marshal=x_to_blk } in
+
+      let create_plist blk_id = 
+        x_to_buf ([],None) |> fun (buf,off) -> 
         let pl = { hd=blk_id;
                    tl=blk_id;
-                   tl_contents=elts;
-                   tl_buffer;
-                   tl_nxt;
-                   tl_dirty=false; (* but we must write to disk *)
+                   buffer=buf;
+                   off;
+                   nxt_is_none=true;
+                   dirty=true;
                  }
         in
-        let blk = buf_to_blk (fst tl_buffer) in
+        let blk = buf_to_blk buf in
         write ~blk_id ~blk >>= fun () ->
-        return pl
+        return {pl with dirty=false}
       in
 
-      let marshal (elts,nxt) = 
-        x_to_buffer ~elts ~nxt |> fun (buf,_) -> buf_to_blk buf
-      in
-      let unmarshal blk = blk_to_buf blk |> buf_to_x in      
-      {
-        add=failwith "";
-        sync_tl=failwith"";
-        empty;
-        marshal;
-        unmarshal
-      }
+      (* working with_state *)
 
+      let clear_nxt_blk blk_id = 
+        x_to_blk ([],None) |> fun blk ->
+        write ~blk_id ~blk
+      in
+
+      (* invariant: if tl not written to disk yet, then tl_dirty is true *)
+      let sync_tl ~state = 
+        let { tl; buffer; dirty; _ } = state in
+        match dirty with
+          | true -> (
+              write ~blk_id:tl ~blk:(buf_to_blk buffer) >>= fun () ->
+              return {state with dirty=false })
+          | false -> return state
+      in
+
+      (* add element in tl; don't use blk_dev *)
+      let add_elt ~state ~elt = 
+        let buf,off = state.buffer,state.off in
+        assert(can_fit ~off ~n:(2*max_elt_sz));
+        (* write an elt followed by a None, and update the pointer offset *)
+        (buf,off) |> m_elt (Some elt) |> fun (buf,off) -> 
+        (buf,off) |> m_elt None |> fun (buf,_) -> 
+        (* note the offset is just before the none; FIXME maybe can
+           optimize by only writing None once per blk *)
+        {state with buffer=buf; off; dirty=true}
+      in
+
+      (* don't use blk_dev *)
+      let set_nxt nxt buf =
+        (* update nxt first *)
+        (buf,0) |> m_blk_id nxt |> fun (buf,_) -> 
+        buf
+      in        
+
+      (* don't use blk_dev *)
+      let move_to_nxt ~hd ~nxt ~buf ~off ~dirty =
+        (* assert(buf_to_x state.buffer |> snd = nxt); *)
+        { hd; tl=nxt; buffer=buf; off; nxt_is_none=true; dirty }
+      in
+
+      let add = 
+        fun ~(with_state:(('a,blk_id,buf)plist,t) with_state) ->
+        fun ~nxt ~elt ->
+          with_state.with_state (fun ~state ~set_state ->
+            let { off; _ } = state in
+            match can_fit ~off ~n:(2*max_elt_sz) with
+            | true -> (
+                add_elt ~state ~elt |> fun state ->
+                set_state state >>= fun () ->
+                return (Some nxt))
+            | false -> (
+                (* write the new blk first *)
+                x_to_buf ([elt],None) |> fun (buf,off) ->
+                write ~blk_id:nxt ~blk:(buf_to_blk buf) >>= fun () ->
+                (* write old blk with nxt *)
+                let blk_id = state.tl in
+                write ~blk_id ~blk:(state.buffer |> set_nxt (Some nxt) |> buf_to_blk) >>= fun () ->
+                move_to_nxt ~hd:state.hd ~nxt ~buf ~off ~dirty:false |> fun state ->
+                set_state state >>= fun () ->
+                return None))
+      in
+      let ret = ops1,create_plist,fun with_state -> add ~with_state in
+      ret
+
+  let _ : monad_ops:t monad_ops ->
+buf_ops:buf buf_ops ->
+blk_ops:blk blk_ops ->
+blk_dev_ops:(blk_id, blk, t) blk_dev_ops ->
+marshal_info:('b, blk_id, blk, buf) marshal_info ->
+('b, blk_id, blk) plist_marshal_ops *
+(blk_id -> (('c, blk_id, buf) plist, t) m) *
+((('a, blk_id, buf) plist, t) with_state ->
+ nxt:blk_id -> elt:'b -> (blk_id option, t) m) = make
 end
 
--- src/plist_intf.ml
+++ src/plist_intf.ml
@@ -1,27 +1,30 @@
-
-
 type ('a,'blk_id,'buf) plist = {
-  hd          :'blk_id;
-  tl          :'blk_id;
-  tl_contents :'a list; (** needed? *) 
-  tl_buffer   :'buf*int; (** including nxt pointer *)
-  tl_nxt      :'blk_id option; (** store in first 8 bytes *)
-  tl_dirty    :bool; (** may not have been written to disk *)
+  hd             : 'blk_id;
+  tl             : 'blk_id;
+  buffer      : 'buf;
+  off         : int;
+  nxt_is_none : bool; 
+  dirty       : bool; (** may not have been written to disk *)
+}
+(** NOTE modifications take place in the tl block *)
+
+  (* tl_contents : 'a list; (\** needed? *\)  *)
+
+(* FIXME we need to ensure buffer and tl_nxt_nil are coherent; we need
+   to identify tl_nxt=None without unmarshalling from the buffer *)
+
+type ('a,'blk_id,'blk) plist_marshal_ops = {  
+  unmarshal : 'blk->'a list * 'blk_id option; (** internal *)
+  marshal   : 'a list * 'blk_id option->'blk; (** internal *)  
 }
 
+type ('a,'buf,'blk_id,'blk,'t) plist_init_ops = {  
+  create_plist : 'blk_id -> (('a,'blk_id,'buf)plist,'t)m;
+}
 
-type ('a,'buf,'blk_id,'blk,'t) plist_ops = {
-  add       : ?force_nxt:[`Before | `After | `False ] -> nxt_blk_id:'blk_id -> elt:'a -> ('blk_id option,'t) m;
-  sync_tl   :unit -> (unit,'t)m; 
-
-  empty     :'blk_id -> (('a,'blk_id,'buf)plist,'t)m;
-
-  unmarshal :'blk->'a list * 'blk_id option; (** internal *)
-  marshal   :'a list * 'blk_id option->'blk; (** internal *)  
-
-  (* empty_node_as_blk:'blk;  *)
-  (* Used to initialize the nxt block *)
-  (* initialize_blk: 'blk_id -> 'a list -> (unit,'t)m; *)
+type ('a,'buf,'blk_id,'blk,'t) plist_ops = {  
+  add     : nxt:'blk_id -> elt:'a -> ('blk_id option,'t) m;
+  sync_tl : unit -> (unit,'t)m; 
 }
 (** add : The blk_id is returned if it is not used; another variant
    assumes an alloc function. We don't assume the nxt_blk is clean -
@@ -33,8 +36,11 @@ type ('a,'buf,'blk_id,'blk,'t) plist_ops = {
    doesn't fit, we are forced to move anyway).
 
 sync_tl : we automatically sync before moving to a new tl
-
-empty : create an empty list in a given blk_id
-
 *)
+
+
+
+  (* empty_node_as_blk:'blk;  *)
+  (* Used to initialize the nxt block *)
+  (* initialize_blk: 'blk_id -> 'a list -> (unit,'t)m; *)
 
