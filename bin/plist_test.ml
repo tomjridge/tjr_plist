c8df81a04434ce1fa977a126f3660dcd5352bc96
Tom Ridge <tom.j.ridge@googlemail.com>
2020-01-20 17:29:40 +0000

    +

--- bin/plist_test.ml
+++ bin/plist_test.ml
@@ -1,6 +1,34 @@
 open Plist_examples
 
-module T = Test()
+type a = 
+  | A1_ocaml_marshal
+  | A2_binprot
+
+let map_a = function
+  | 1 -> A1_ocaml_marshal
+  | 2 -> A2_binprot
+  | _ -> failwith __LOC__
+
+let now = Tjr_profile.now
 
 let _ = 
-  Lwt_main.run (T.main () |> Tjr_monad.With_lwt.to_lwt)
+  let p i = 
+    i |> map_a |> function
+    | A1_ocaml_marshal -> 
+      let module X = Blk_as_bytes_buf_as_bytes.Test() in
+      X.main ()
+    | A2_binprot -> 
+      let module X = Blk_as_ba_buf_as_ba.Test() in
+      X.main ()
+  in
+  let task = 
+    return () >>= fun () ->
+    let t1 = now() in
+    p 1 >>= fun () ->
+    let t2 = now() in
+    p 2 >>= fun () -> 
+    let t3 = now() in
+    Printf.printf "Timings: p1:%d p2:%d\n" (t2-t1) (t3-t2);
+    return ()
+  in    
+  Lwt_main.run ( task |> Tjr_monad.With_lwt.to_lwt)
--- src-examples/plist_examples.ml
+++ src-examples/plist_examples.ml
@@ -2,10 +2,6 @@ open Tjr_plist
 open Plist_intf
 
 type blk_id = Blk_id_as_int.blk_id
-type blk = bytes (* NOTE we must take care to copy the bytes when writing to disk *)
-type buf = Buf_as_bytes.by_buf
-
-let make = make
 
 let monad_ops = Tjr_monad.lwt_monad_ops
 
@@ -14,125 +10,257 @@ let ( >>= ) = monad_ops.bind
 let return = monad_ops.return
 
 
-let buf_ops = Buf_as_bytes.by_buf_ops
+module Blk_as_bytes_buf_as_bytes = struct
+  type blk = bytes (* NOTE we must take care to copy the bytes when writing to disk *)
 
-(* FIXME rename Common_blk_ops so it starts with Blk_; also
-   blk_as_string rather than string_blk_ops *)
-let blk_ops = Tjr_fs_shared.Common_blk_ops.bytes_blk_ops
+  (* FIXME rename Common_blk_ops so it starts with Blk_; also
+     blk_as_string rather than string_blk_ops *)
+  let blk_ops = Tjr_fs_shared.Common_blk_ops.bytes_blk_ops
 
-let blk_dev_ops with_map = 
-  Blk_dev_factory.(make A3_bytes_4096_lwt_mem)
-  |> function | (R3 k) -> k with_map
-              | _ -> failwith __LOC__
-
-let m_any max_sz (x:'a) (buf,off) = 
-  (* Printf.printf "Buffer size: %d; off:%d\n" (Bytes.length buf) off; *)
-  let n : int = Marshal.to_buffer buf off max_sz x [] in 
-  (buf,off+n)
-
-let u_any buf off : 'a * int = 
-  (* Printf.printf "Reading at offset %d\n" off; *)
-  let i : 'a = Marshal.from_bytes buf off in
-  (* FIXME Marshal should have a from_buffer which returns value and
-     number of bytes consumed *)
-  let n = Marshal.total_size buf off in
-  (i,off+n)
+  type buf = Buf_as_bytes.by_buf
 
 
-module Int_plist = struct
+  let make = make
 
-  let max_sz = 32
+  let buf_ops = Buf_as_bytes.by_buf_ops
 
-  (* FIXME these max sizes are guessed upper bounds *)
-  (* TODO we also need a version with bin_prot marshalling, for size
-     and speed *)
-  let marshal_info : (int,blk_id,blk,buf)plist_marshal_info = {
-    max_elt_sz=max_sz;
-    max_blk_id_sz=max_sz;
-    m_elt=m_any max_sz;
-    u_elt=u_any;
-    m_blk_id=m_any max_sz;
-    u_blk_id=u_any;
-    blk_to_buf=(fun x -> x);
-    buf_to_blk=(fun x -> Bytes.copy x);
-  }
+  let blk_dev_ops with_map = 
+    Blk_dev_factory.(make A3_bytes_4096_lwt_mem
+                     |> function | (R3 k) -> k with_map
+                                 | _ -> failwith __LOC__)
 
-  (* NOTE this gets filled in later, once we have the blk/buf
-     marshalling function *)
-  (* let check_blk = ref (fun buf -> true) *)
+  let m_any max_sz (x:'a) (buf,off) = 
+    (* Printf.printf "Buffer size: %d; off:%d\n" (Bytes.length buf) off; *)
+    let n : int = Marshal.to_buffer buf off max_sz x [] in 
+    (buf,off+n)
 
-  let make with_mem = 
-    let blk_dev_ops = blk_dev_ops with_mem in
-    (* let write ~blk_id ~blk = 
-     *   assert(!check_blk blk);
-     *   blk_dev_ops.write ~blk_id ~blk
-     * in
-     * let read ~blk_id = 
-     *   blk_dev_ops.read ~blk_id >>= fun blk ->
-     *   assert(!check_blk blk);
-     *   return blk
-     * in
-     * let blk_dev_ops = { blk_dev_ops with read; write } in *)
-    make ~monad_ops ~buf_ops ~blk_ops ~blk_dev_ops ~marshal_info
+  let u_any buf off : 'a * int = 
+    (* Printf.printf "Reading at offset %d\n" off; *)
+    let i : 'a = Marshal.from_bytes buf off in
+    (* FIXME Marshal should have a from_buffer which returns value and
+       number of bytes consumed *)
+    let n = Marshal.total_size buf off in
+    (i,off+n)
 
-  let _ = make
-end
 
-module Test() : sig
-  val main : unit -> (unit, lwt) m
-end = struct
+  module Int_plist = struct
+    let max_sz = 32
 
-  (** In-mem blk dev, mutated directly *)
-  let mem = ref (Tjr_map.With_pervasives_compare.empty ())
+    (* FIXME these max sizes are guessed upper bounds *)
+    (* TODO we also need a version with bin_prot marshalling, for size
+       and speed *)
+    let marshal_info : (int,blk_id,blk,buf)plist_marshal_info = {
+      max_elt_sz=max_sz;
+      max_blk_id_sz=max_sz;
+      m_elt=m_any max_sz;
+      u_elt=u_any;
+      m_blk_id=m_any max_sz;
+      u_blk_id=u_any;
+      blk_to_buf=(fun x -> x);
+      buf_to_blk=(fun x -> Bytes.copy x);
+    }
 
-  let with_mem : ('a,lwt)with_state = 
-    let with_state f = f ~state:!mem ~set_state:(fun s -> mem:=s; return ()) in
-    { with_state }
+    (* NOTE this gets filled in later, once we have the blk/buf
+       marshalling function *)
+    (* let check_blk = ref (fun buf -> true) *)
 
-  let _ = with_mem
+    let make with_mem = 
+      let blk_dev_ops = blk_dev_ops with_mem in
+      (* let write ~blk_id ~blk = 
+       *   assert(!check_blk blk);
+       *   blk_dev_ops.write ~blk_id ~blk
+       * in
+       * let read ~blk_id = 
+       *   blk_dev_ops.read ~blk_id >>= fun blk ->
+       *   assert(!check_blk blk);
+       *   return blk
+       * in
+       * let blk_dev_ops = { blk_dev_ops with read; write } in *)
+      make ~monad_ops ~buf_ops ~blk_ops ~blk_dev_ops ~marshal_info
 
-  let int_plist = Int_plist.make with_mem
+    let _ = make
+  end
 
-  let _ = int_plist
+  module Test() : sig
+    val main : unit -> (unit, lwt) m
+  end = struct
 
-  let { m_u_ops=_; extra_ops={create_plist; read_plist}; plist_ops=add_sync } = int_plist
+    (** In-mem blk dev, mutated directly *)
+    let mem = ref (Tjr_map.With_pervasives_compare.empty ())
 
-  (* let _ = 
-   *   Int_plist.check_blk := (fun blk -> let _ = m_u_ops.unmarshal blk in true) *)
+    let with_mem : ('a,lwt)with_state = 
+      let with_state f = f ~state:!mem ~set_state:(fun s -> mem:=s; return ()) in
+      { with_state }
 
-  let num_elts = 10_000
+    let _ = with_mem
 
-  let main () = 
-    create_plist (Blk_id_as_int.of_int 0) >>= fun plist -> 
-    let state = ref plist in
-    let with_state : ((int, blk_id, buf) plist,lwt) with_state = 
-      let with_state f = f ~state:!state ~set_state:(fun s -> 
-          state:=s; return ())
+    let int_plist = Int_plist.make with_mem
+
+    let _ = int_plist
+
+    let { m_u_ops=_; extra_ops={create_plist; read_plist}; plist_ops=add_sync } = int_plist
+
+    (* let _ = 
+     *   Int_plist.check_blk := (fun blk -> let _ = m_u_ops.unmarshal blk in true) *)
+
+    let num_elts = 10_000
+
+    let main () = 
+      create_plist (Blk_id_as_int.of_int 0) >>= fun plist -> 
+      let state = ref plist in
+      let with_state : ((int, blk_id, buf) plist,lwt) with_state = 
+        let with_state f = f ~state:!state ~set_state:(fun s -> 
+            state:=s; return ())
+        in
+        {with_state}
       in
-      {with_state}
-    in
-    let {add;sync} = add_sync with_state in
-    (1,0) |> iter_k (fun ~k (min_free_blk_id,n) -> 
-        match n >= num_elts with
-        | true -> 
-          (* NOTE make sure the last block is actually written *)
-          sync () >>= fun () ->
-          return min_free_blk_id
-        | false -> 
-          add
-            ~nxt:(Blk_id_as_int.of_int min_free_blk_id)
-            ~elt:n
-          >>= fun x -> 
-          match x with 
-          | None -> k (min_free_blk_id+1,n+1)
-          | Some _ -> k (min_free_blk_id,n+1))
-    >>= fun min_free_blk_id ->     
-    Printf.printf "Finished with next free blk: %d\n" min_free_blk_id;
-    read_plist (Blk_id_as_int.of_int 0) >>= fun xs ->
-    xs |> List.map (fun (elts,nxt_) -> elts) |> List.concat |> fun xs ->
-    Printf.printf "Read %d elts from disk\n" (List.length xs);
-    assert(num_elts = List.length xs);
-    return ()
+      let {add;sync} = add_sync with_state in
+      (1,0) |> iter_k (fun ~k (min_free_blk_id,n) -> 
+          match n >= num_elts with
+          | true -> 
+            (* NOTE make sure the last block is actually written *)
+            sync () >>= fun () ->
+            return min_free_blk_id
+          | false -> 
+            add
+              ~nxt:(Blk_id_as_int.of_int min_free_blk_id)
+              ~elt:n
+            >>= fun x -> 
+            match x with 
+            | None -> k (min_free_blk_id+1,n+1)
+            | Some _ -> k (min_free_blk_id,n+1))
+      >>= fun min_free_blk_id ->     
+      Printf.printf "Finished with next free blk: %d\n" min_free_blk_id;
+      read_plist (Blk_id_as_int.of_int 0) >>= fun xs ->
+      xs |> List.map (fun (elts,nxt_) -> elts) |> List.concat |> fun xs ->
+      Printf.printf "Read %d elts from disk\n" (List.length xs);
+      assert(num_elts = List.length xs);
+      return ()
 
-  let _ = main
+    let _ = main
+  end
+
 end
+
+
+module Blk_as_ba_buf_as_ba = struct
+  type blk = Buf_as_bigarray.ba_buf (* NOTE we must take care to copy the bytes when writing to disk *)
+
+  let blk_ops = Blk_factory.(make A3_ba_4096 |> function R3 ops -> ops | _ -> failwith __LOC__)
+
+  type buf = Buf_as_bigarray.ba_buf
+
+  let buf_ops = Buf_as_bigarray.ba_buf_ops
+
+  module Int_binprot = struct
+
+    module Int_option = struct
+      open Bin_prot.Std
+      type int_opt = int option [@@deriving bin_io]
+    end
+    
+    let max_sz = 10 (* 1 for option; 1 for int tag; 8 for int *)
+
+    let m_elt (x:int option) (buf,off) = 
+      Int_option.bin_write_int_opt buf ~pos:off x |> fun off' -> 
+      (buf,off')
+
+    let m_blk_id blk_id = 
+      blk_id |> (function None -> None | Some x -> Some (Blk_id_as_int.to_int x))
+      |> m_elt 
+
+    let u_elt buf off = 
+      let pos_ref = ref off in
+      Int_option.bin_read_int_opt buf ~pos_ref |> fun r ->
+      (r,!pos_ref)
+
+    let u_blk_id buf off = 
+      u_elt buf off |> fun (x,off) -> 
+      x |> (function None -> None | Some x -> Some(Blk_id_as_int.of_int x)) |> fun x -> 
+      (x,off)
+                                             
+    let marshal_info : (int,blk_id,blk,buf)plist_marshal_info = {
+      max_elt_sz=max_sz;
+      max_blk_id_sz=max_sz;
+      m_elt;
+      u_elt;
+      m_blk_id;
+      u_blk_id;
+      blk_to_buf=(fun x -> Buf_as_bigarray.ba_copy x);
+      buf_to_blk=(fun x -> Buf_as_bigarray.ba_copy x);
+    }
+
+    let blk_dev_ops with_map = 
+      Blk_dev_factory.(make A4_ba_4096_lwt_mem
+                       |> function | (R4 k) -> k with_map
+                                   | _ -> failwith __LOC__)
+
+    let make with_mem = 
+      let blk_dev_ops = blk_dev_ops with_mem in
+      make ~monad_ops ~buf_ops ~blk_ops ~blk_dev_ops ~marshal_info
+
+
+
+  end
+
+  module Test() : sig
+    val main : unit -> (unit, lwt) m
+  end = struct
+
+    (** In-mem blk dev, mutated directly *)
+    let mem = ref (Tjr_map.With_pervasives_compare.empty ())
+
+    let with_mem : ('a,lwt)with_state = 
+      let with_state f = f ~state:!mem ~set_state:(fun s -> mem:=s; return ()) in
+      { with_state }
+
+    let _ = with_mem
+
+    let int_plist = Int_binprot.make with_mem
+
+    let _ = int_plist
+
+    let { m_u_ops=_; extra_ops={create_plist; read_plist}; plist_ops=add_sync } = int_plist
+
+    (* let _ = 
+     *   Int_plist.check_blk := (fun blk -> let _ = m_u_ops.unmarshal blk in true) *)
+
+    let num_elts = 10_000
+
+    let main () = 
+      create_plist (Blk_id_as_int.of_int 0) >>= fun plist -> 
+      let state = ref plist in
+      let with_state : ((int, blk_id, buf) plist,lwt) with_state = 
+        let with_state f = f ~state:!state ~set_state:(fun s -> 
+            state:=s; return ())
+        in
+        {with_state}
+      in
+      let {add;sync} = add_sync with_state in
+      (1,0) |> iter_k (fun ~k (min_free_blk_id,n) -> 
+          match n >= num_elts with
+          | true -> 
+            (* NOTE make sure the last block is actually written *)
+            sync () >>= fun () ->
+            return min_free_blk_id
+          | false -> 
+            add
+              ~nxt:(Blk_id_as_int.of_int min_free_blk_id)
+              ~elt:n
+            >>= fun x -> 
+            match x with 
+            | None -> k (min_free_blk_id+1,n+1)
+            | Some _ -> k (min_free_blk_id,n+1))
+      >>= fun min_free_blk_id ->     
+      Printf.printf "Finished with next free blk: %d\n" min_free_blk_id;
+      read_plist (Blk_id_as_int.of_int 0) >>= fun xs ->
+      xs |> List.map (fun (elts,nxt_) -> elts) |> List.concat |> fun xs ->
+      Printf.printf "Read %d elts from disk\n" (List.length xs);
+      assert(num_elts = List.length xs);
+      return ()
+
+    let _ = main
+  end
+
+end
+
